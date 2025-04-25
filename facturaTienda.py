afac9b00ea2ab8091973499db11f54300a2a9ae3
JavierDevPro <gjangsam@gmail.com>
2025-04-25 07:48:32 -0500

    terminacion del proyecto, arreglo de errores en la funcion validacion e implementacion a mejoras del retorno de la funcion de calculo.

--- facturaTienda.py
+++ facturaTienda.py
@@ -1,3 +1,4 @@
+#Declaracion e iniciacion de variables para evitar errores en las condiciones y procesos que evaluen o tomen estos valores
 unitPrice = 0
 productQty = 0
 percentDiscount = 0
@@ -6,9 +7,10 @@ discount = False
 answersList = ["si","s","no","n"]
 #funcion que valide valores numericos que sean numeros y valida que las variables cantidad y precio tengan un valor minimo mayor a 0
 def validacionValNumMinMax(value, process):  
-  while True:
+  while True:#ciclo que busca validar y corregir errores en el ingreso de datos segun el proceso en el que va el codigo
+    valid = True#Bandera para evitar que se ejecuten 2 errores seguidos
     try:#ejecutamos un try para que en caso de error no se detenga el programa
-      match(process):
+      match(process):#estructura match muy similar al switch case en otros lenguajes que evalua si se cumple una condicion de un valor exacto
         case 1:
           value = input("Precio unitario: ").strip()
           newValue = float(value)
@@ -21,9 +23,10 @@ def validacionValNumMinMax(value, process):
         case _:
           print("watafak chavales")
     except:
-        print("ERROR: El valor ingresado no debe poseer valores alfanumericos.")    
+        print("ERROR: El valor ingresado no debe poseer valores alfanumericos.")
+        valid = False  
     try:
-      # if not(value.isalpha()):
+      if (not(value.isalpha()) and valid == True): #and value.isdigit()
         if (process == 1 or process == 2):
           if newValue > 0:
              break
@@ -37,12 +40,17 @@ def validacionValNumMinMax(value, process):
     except:
       continue
   return newValue
-#Funcion que calcule el total
+#Funcion que calcule el total y devuelve una lista de la cual expondres mis valores obtenidos
 def totalCalculation(prodQty, unitP, percent):
+  listValues = []
   sub = prodQty * unitP
-  discValue = sub * (percent / 100)
+  discValue = (sub * (percent / 100))
   tot = sub - discValue
-  return tot
+  listValues.append(sub)
+  listValues.append(discValue)
+  listValues.append(tot)
+  return listValues
+#La siguiente seccion es el punto de salida y ejecucion de la funciones anteriores
 print("Ingrese los datos del producto que va a comprar.")
 productName = input("nombre: ")
 unitPrice = validacionValNumMinMax(unitPrice, 1)
@@ -50,16 +58,19 @@ productQty = validacionValNumMinMax(productQty, 2)
 validDiscount = input("desea implementar descuento? Si/No.")#se toma el valor de la respuesta
 while validDiscount.lower() not in answersList:#ciclo que valide que el valor este en la lista de respuestas
   validDiscount = input("Respuesta invalida, desea implementar descuento? Si/No: ")
-if (validDiscount.lower() == "si" or validDiscount.lower() == "s"):
+if (validDiscount.lower() == "si" or validDiscount.lower() == "s"):#si la respuesta fue si se calcula el descuento
   discount = True
   percentDiscount = validacionValNumMinMax(percentDiscount, 3)
+listFinal = totalCalculation(productQty, unitPrice, percentDiscount)#se llama la funcion de calculo
+#se imprimen los datos finalmente calculados
 print("**********************************************************")
 print("Nuevo producto adquirido!")
 print("producto: ", productName)
+print("valor unitario: $ ", unitPrice)
 print("cantidad: ", productQty)
 if (discount==True):
   print("porcentaje descontado: ", percentDiscount,"%")
-  #print("valor descontado: ")
-print("TOTAL: $",totalCalculation(productQty, unitPrice, percentDiscount))
+  print(f"valor descontado: $ {round(listFinal[1])} ")#se redondea el resultado
+  print("subtotal: $ ", listFinal[0])
+print("TOTAL: $ ", listFinal[2])
 print("**********************************************************")
-  
